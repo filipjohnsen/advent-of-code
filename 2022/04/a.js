86a0f88f4408d2be9c040709eebbc08389edbf71
Filip Johnsen <filipjohnsen97@gmail.com>
2022-12-04 09:13:34 +0100

    nr 4

---
+++ 2022/04/a.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+const file = fs.readFileSync('./input.txt', 'utf8');
+
+const pairs = file.split('\n');
+
+const overlappingPairs = pairs.filter((pair) => {
+  const [a, b] = pair.split(',');
+
+  const aMin = parseInt(a.split('-')[0]);
+  const aMax = parseInt(a.split('-')[1]);
+  const bMin = parseInt(b.split('-')[0]);
+  const bMax = parseInt(b.split('-')[1]);
+
+  if ((aMin >= bMin && aMax <= bMax) || (bMin >= aMin && bMax <= aMax)) {
+    return pair;
+  }
+});
+
+console.log(overlappingPairs.length);
