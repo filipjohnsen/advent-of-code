c5bd4908c29928a321e5dbe8b780abb70dec065b
Filip Johnsen <filipjohnsen97@gmail.com>
2022-12-05 23:01:53 +0100

    fixed

--- 2022/filip/01/b.js
+++ 2022/filip/01/b.js
@@ -6,7 +6,7 @@ const result = file
   .split('\n\n')
   .map((group) => group.split('\n').reduce((acc, curr) => acc + parseInt(curr), 0))
   .sort((a, b) => a - b)
-  .splice(elfGroups.length - 3, 3)
+  .splice(file.split('\n\n').length - 3, 3)
   .reduce((acc, curr) => acc + curr, 0);
 
 console.log(result);
