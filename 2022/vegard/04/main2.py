0c69072e4f4073d87c1bbecbf8da0f59560ca05c
Vegard Nerhus <vegardn@studix.com>
2022-12-05 23:51:16 +0100

    Vegard day 4

--- 2022/vegard/04/main2.py
+++ 2022/vegard/04/main2.py
@@ -5,8 +5,8 @@ for line in file:
     elf1, elf2 = line.strip().split(",")
     elf1 = [int(n) for n in elf1.split("-")]
     elf2 = [int(n) for n in elf2.split("-")]
-    numbers1 = list(range(elf1[0], elf1[1]))
-    numbers2 = list(range(elf2[0], elf2[1]))
+    numbers1 = list(range(elf1[0], elf1[1]+1))
+    numbers2 = list(range(elf2[0], elf2[1]+1))
     total += int(any([number//number for number in numbers1 if numbers2.count(number) > 0]))
 
 print(total)
