22fd86e6abd75e54cd495d170964cf9fca2db457
Filip Johnsen <filipjohnsen97@gmail.com>
2022-12-06 17:43:44 +0100

    day 6

---
+++ 2022/filip/06/a.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+const file = fs.readFileSync('./input.txt', 'utf8');
+
+const packets = file.split('');
+
+let signal = [];
+let start = 0;
+let idx = 0;
+
+for (const packet of packets) {
+  if (!signal.includes(packet)) {
+    signal.push(packet);
+  } else {
+    signal = [];
+  }
+
+  if (signal.length === 4) {
+    start = idx;
+    break;
+  }
+
+  idx++;
+}
+
+console.log(start);
